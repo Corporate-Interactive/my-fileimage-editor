451dd681ead0d06f5d3d172a8c7056e6c78edc2c
VuCIT <vu.tran@corporateinteractive.com.au>
2025-05-15 15:03:34 +1000

    Updated package build

--- package/react-image-editor-q/src/index.js
+++ package/react-image-editor-q/src/index.js
@@ -1,13 +1,16 @@
 /** Internal Dependencies */
-// import FilerobotImageEditor from './components/AssemblyPoint';
-// import { TABS_IDS, TOOLS_IDS } from './utils/constants';
-
-// import FilerobotImageEditor from './components/AssemblyPoint';
-import FilerobotImageEditor from './components/AssemblyPoint/index.jsx';
-
+import FilerobotImageEditor from './components/AssemblyPoint';
 import { TABS_IDS, TOOLS_IDS } from './utils/constants';
 
-
 export { TABS_IDS as TABS, TOOLS_IDS as TOOLS };
-
 export default FilerobotImageEditor;
+
+// import FilerobotImageEditor from './components/AssemblyPoint';
+// import FilerobotImageEditor from './components/AssemblyPoint/index.jsx';
+
+// import { TABS_IDS, TOOLS_IDS } from './utils/constants';
+
+
+// export { TABS_IDS as TABS, TOOLS_IDS as TOOLS };
+
+// export default FilerobotImageEditor;
